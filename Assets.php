18436ca543b70e12a39d6db10b73c2f4c22bdb05
denis909 <denis909@mail.ru>
2019-09-07 18:14:48 +0300

    n/a

--- Assets.php
+++ Assets.php
@@ -19,9 +19,7 @@ class Assets extends AssetBundle
         'vendor/bootstrap/js/bootstrap.bundle.min.js',
         'vendor/jquery-easing/jquery.easing.min.js',
         'js/sb-admin-2.min.js',
-        'vendor/chart.js/Chart.min.js',
-        'js/demo/chart-area-demo.js',
-        'js/demo/chart-pie-demo.js'
+        'vendor/chart.js/Chart.min.js'
     ];
     
     public $depends = [
