f4b4a7ccc747a5fd6ddb6f541ba311fb0f5d6369
denis909 <denis909@mail.ru>
2019-09-14 22:29:47 +0300

    n/a

--- Assets.php
+++ Assets.php
@@ -27,7 +27,7 @@ class Assets extends AssetBundle
         'yii\web\JqueryAsset',
         'yii\bootstrap4\BootstrapAsset',
         'yii\bootstrap4\BootstrapPluginAsset',
-        'denis909\yii\glyphicons\GlyphiconsAsset',
+        'denis909\glyphicons\GlyphiconsAsset',
         AssetsLocal::class
     ];
 
