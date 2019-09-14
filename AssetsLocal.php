39d42ab0a21a147b2f44f5fbec8753ca6712cc5d
denis909 <denis909@mail.ru>
2019-09-14 22:43:12 +0300

    n/a

--- AssetsLocal.php
+++ AssetsLocal.php
@@ -7,7 +7,7 @@ use yii\web\AssetBundle;
 class AssetsLocal extends AssetBundle
 {
 
-    public $sourcePath = '@vendor/denis909/yii-theme-sbadmin2/assets';
+    public $sourcePath = '@vendor/denis909/yii2-theme-sbadmin2/assets';
 
     public $css = ['style.css'];
 
