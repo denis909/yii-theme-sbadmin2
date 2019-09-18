2f62907a0daae8e228b6c051df1e1f4bf25af27e
denis909 <denis909@mail.ru>
2019-09-18 22:10:39 +0300

    n/a

--- MainMenu.php
+++ MainMenu.php
@@ -9,12 +9,12 @@ class MainMenu extends \denis909\yii\Menu
 
     public $theme;
 
+    public $activateParents = true;
+
     public $options = [
         'id' => 'accordionSidebar',
         'class' => 'navbar-nav bg-gradient-primary sidebar sidebar-dark accordion'
     ];
-
-    public $encodeLabels = false;
 
     public $itemOptions = ['class' => 'nav-item'];
 
@@ -22,15 +22,26 @@ class MainMenu extends \denis909\yii\Menu
 
     public $iconTemplate = '<i class="{icon}"></i> <span>{label}</span>';
 
-    public $submenuTemplate = '<div id="{id}" class="collapse"><div class="bg-white py-2 collapse-inner rounded">{items}</div></div>';
+    public $submenuTemplate = '<div id="{id}" class="collapse">{items}</div>';
 
     public $labelTemplate = '<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#{id}">{label}</a>';
 
-    public $submenuItem = [
-        'options' => [
-            'tag' => false
-        ],
-        'template' => '<a class="collapse-item" href="{url}">{label}</a>' 
-    ];
+    public $submenuClass = MainMenu2::class;
+
+    public function renderItem($item)
+    {
+        $isActive = array_key_exists('active', $item) && $item['active'] ? true : false;
+
+        $return = parent::renderItem($item);
+
+        if ($isActive && !empty($item['_items']))
+        {
+            $return = str_replace('class="collapse"', 'class="collapse show"', $return);
+
+            $return = str_replace('class="nav-link"', 'class="nav-link collapse"', $return);
+        }
+
+        return $return;
+    }
     
 }
---
+++ MainMenu2.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace denis909\sbadmin2;
+
+class MainMenu2 extends \denis909\yii\Menu
+{
+
+    public $options = ['tag' => 'div', 'class' => 'bg-white py-2 collapse-inner rounded'];
+
+    public $itemOptions = ['tag' => false];
+
+    public $linkOptions = ['class' => 'collapse-item'];
+
+    public $iconTemplate = '<i class="{icon}"></i> <span>{label}</span>';
+
+    protected function renderItem($item)
+    {
+        $isActive = array_key_exists('active', $item) && $item['active'] ? true : false;
+
+        $return = parent::renderItem($item);
+
+        if ($isActive)
+        {
+             $return = str_replace('class="collapse-item"', 'class="collapse-item active"', $return);
+        }
+
+        return $return;
+    }
+
+}
