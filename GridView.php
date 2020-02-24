e846bedef4036afee17b4c73c6a97c24f7aafc3a
denis909 <denis909@mail.ru>
2020-02-24 14:51:25 +0300

    n/a

--- GridView.php
+++ GridView.php
@@ -5,7 +5,10 @@ namespace denis909\sbadmin2;
 class GridView extends \yii\grid\GridView
 {
 
-    public $layout = '<div class="mb-2">{items}</div><div class="row"><div style="line-height: 38px;" class="col-sm-12 col-md-5">{summary}</div><div class="col-sm-12 col-md-7">{pager}</div></div>';
+    public $layout = '<div class="mb-2 overflow-auto">{items}</div>' 
+        . '<div class="row">'
+            . '<div style="line-height: 38px;" class="col-sm-12 col-md-5">{summary}</div><div class="col-sm-12 col-md-7">{pager}</div>' 
+        . '</div>';
 
     public $tableOptions = ['class' => 'table table-striped table-bordered'];
 
